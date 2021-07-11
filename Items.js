b21b9b0376c617bad56a9f06184084c067d95d3b
Ashwin Chandran <ashwinchandran13@gmail.com>
2021-07-11 11:46:01 +0530

    request setup/first request

---
+++ Items.js
@@ -0,0 +1,16 @@
+let items = [
+    {
+        id: '1',
+        name: 'Item One'
+    },
+    {
+        id: '2',
+        name: 'Item two'
+    },
+    {
+        id: '3',
+        name: 'Item Three'
+    },
+]
+
+module.exports = items;
